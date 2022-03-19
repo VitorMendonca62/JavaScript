fc68cce795e90ecf47e2d596e39c533e0228abf2
VitorMendonca62 <vitorqueiroz325@gmail.com>
2022-03-19 11:20:50 -0300

    Compactaçãoe e melhoramento do cogido

--- Projetos/Calculadora-Avançada/main.js
+++ Projetos/Calculadora-Avançada/main.js
@@ -1,45 +1,30 @@
-var resultado = document.getElementById("resultado")
-var numeros;
-var arrayNumeros = new Array()
+const resultado = document.getElementById("resultado")
+const arrayNumeros = new Array()
 
 function escreveNumero(numeroEscolhido) {
     arrayNumeros.push(numeroEscolhido)
     resultado.value += numeroEscolhido
 }
-function escreveDecimal() {
-    if (resultado.value.length == 0) {
-        resultado.value += "0."
-    } else {
-        resultado.value += "."
-    }
-}
+const escreveDecimal = () => resultado.value.length === 0 ? resultado.value += "0." : resultado.value += "."
+    
 function escreveOperadores(operador) {
-    if (resultado.value.length == 0) {
+    if (resultado.value.length === 0) {
         alert("[ERRO] Adicione um número")
     } else {
         resultado.value += operador
         arrayNumeros.push(operador) 
     }
 }
-function mostrarResultado() {
-    if (resultado.value.length == 0) {
-        alert("[ERRO] Adicione um número")
-    } else {
-        resultado.value = eval(resultado.value)
-    }
-}
-function limparTela() {
-    resultado.value = ""
-}
+const mostrarResultado = () => resultado.value.length === 0 ? alert("[ERRO] Adicione um número") : 
+resultado.value = eval(resultado.value)
+
+const limparTela = () => resultado.value = ""
+
 function limparUltimoNumero() {
     if (resultado.value.length == 0) {
         alert("[ERRO] Adicione um número")
     } else {
-        console.log(arrayNumeros)
-        var ultimoNumero = arrayNumeros.pop()
-        console.log(arrayNumeros)
+        const ultimoNumero = arrayNumeros.pop()
         resultado.value = resultado.value.replace(ultimoNumero, "")
     }
-
-
 }
