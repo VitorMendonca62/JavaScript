de9489a4142db03a1bc665334628d02a5f74b293
VitorMendonca62 <vitorqueiroz325@gmail.com>
2022-10-29 19:26:22 -0300

    atualizacao calculadora

--- Projetos/simple/advanced-calculator/main.js
+++ Projetos/simple/advanced-calculator/main.js
@@ -1,65 +1,67 @@
 const resultado = document.getElementById("resultado");
 const operadores = ["+", "-", "/", "*"];
-const arrayNumeros = new Array();
+const algarismos = [...document.getElementsByClassName("algarismos")];
+
+function verificaSeForZero() {
+  resultado.value.length === 0 ? true : false;
+}
+
+function verificaAlgarismo(algarismoEscolhido) {
+  const algarismo = algarismoEscolhido || event.target.innerText;
+  operadores.forEach((operador) =>
+    operador === algarismo ? escreveOperadores(algarismo) : false
+  );
+  if (!isNaN(algarismo) || !isNaN(algarismoEscolhido)) {
+    escreveNumero(algarismo);
+  }
+}
 
 function escreveNumero(numeroEscolhido) {
-  resultado.value == 0
+  resultado.value == "0"
     ? (resultado.value = numeroEscolhido)
     : (resultado.value += numeroEscolhido);
-  arrayNumeros.push(numeroEscolhido);
 }
-const escreveDecimal = () =>
+
+function escreveOperadores(operador) {
+  const ultimoAlgarismo = resultado.value.split("").pop();
+  !!isNaN(ultimoAlgarismo) ? false : (resultado.value += operador);
+}
+
+function escreveDecimal() {
   resultado.value.length === 0
     ? (resultado.value += "0.")
     : (resultado.value += ".");
-
-function escreveOperadores(operador) {
-  if (resultado.value.length === 0) {
-    alert("[ERRO] Adicione um número");
-  } else {
-    const valorFinal = resultado.value[resultado.value.length - 1];
-    const isOperador = operadores.filter((op) => op === valorFinal);
-    if (isOperador.length) {
-      alert("[ERRO] Adicione um número");
-    } else {
-      resultado.value += operador;
-      arrayNumeros.push(operador);
-    }
-  }
 }
-const mostrarResultado = () =>
-  resultado.value.length === 0
-    ? alert("[ERRO] Adicione um número")
-    : (resultado.value = eval(resultado.value));
+
+const mostrarResultado = () => (resultado.value = eval(resultado.value));
 
 const limparTela = () => (resultado.value = "");
 
 function limparUltimoNumero() {
-  if (resultado.value.length == 0) {
-    alert("[ERRO] Adicione um número");
-  } else {
-    const ultimoNumero = arrayNumeros.pop();
-    resultado.value = resultado.value.replace(ultimoNumero, "");
-  }
+  const ultimoNumero = arrayNumeros.pop();
+  resultado.value = resultado.value.replace(ultimoNumero, "");
 }
 
 function teclaPressioanda(event) {
   const tecla = event.key;
 
   if (!isNaN(tecla)) {
-    escreveNumero(tecla);
+    verificaAlgarismo(tecla);
   }
   if (tecla == "*" || tecla == "/" || tecla == "+" || tecla == "-") {
-    escreveOperadores(tecla);
+    verificaSeForZero() ? false : verificaAlgarismo(tecla);
   }
   if (tecla === ".") {
     escreveDecimal();
   }
   if (tecla === "Enter") {
-    mostrarResultado();
+    verificaSeForZero() ? false : mostrarResultado(tecla);
   }
   if (tecla === "Backspace") {
-    limparUltimoNumero();
+    verificaSeForZero() ? false : limparUltimoNumero(tecla);
   }
 }
 document.addEventListener("keydown", teclaPressioanda);
+algarismos.forEach((botao) =>
+  botao.addEventListener("click", () => verificaAlgarismo())
+);
